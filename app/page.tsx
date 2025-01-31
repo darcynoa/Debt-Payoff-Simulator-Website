43916738d73e774f8bc82bdd2d601a4ccc7e7d5b
Noah D'Arcy <noahdarcy.dev@gmail.com>
2025-01-31 04:40:26 -0500

    Display snowball and avalanche results

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
+import { log } from "console";
+
 
 export default async function Home() {
-  let data = await fetch('http://127.0.0.1:5000/api/data');
-  let debtData = await data.json()
+  let data = await fetch('http://127.0.0.1:5000/api/avalanche');
+  let avalanche = await data.json()
+  console.log('avalanche :', avalanche)
+  data = await fetch('http://127.0.0.1:5000/api/snowball');
+  let snowball = await data.json()
+  console.log('snowball:', snowball)
 
-  const totalAmountPaid = Math.round(debtData.summary.total_amount_paid)
-  const totalMonths = debtData.summary.total_months
+  const avalancheTotalAmountPaid = Math.round(avalanche.summary.total_amount_paid)
+  const avalancheTotalMonths = avalanche.summary.total_months
 
-  // DELETE # Can we have initial loading call the API but further updates use Server Actions?
-  
+  const snowballTotalAmountPaid = Math.round(snowball.summary.total_amount_paid)
+  const snowballTotalMonths = snowball.summary.total_months
+
   return (
-    <div className="">
-      <h1 className="text-[4rem]">With the {debtData.summary.method} method</h1>
-      <p>You will pay off ${totalAmountPaid} (approximately) over the course of {totalMonths} months</p>
+    <div className="grid grid-cols-2 gap-6">
+      <div className="">
+        <h1 className="text-[4rem]">With the {avalanche.summary.method} method</h1>
+        <p>You will pay off <strong>${avalancheTotalAmountPaid}</strong> (approximately) over the course of {avalancheTotalMonths} months</p>
+      </div>
+      <div className="">
+        <h1 className="text-[4rem]">With the {snowball.summary.method} method</h1>
+        <p>You will pay off <strong>${snowballTotalAmountPaid}</strong> (approximately) over the course of {snowballTotalMonths} months</p>
+      </div>
     </div>
   );
 }
 
 
+ 
